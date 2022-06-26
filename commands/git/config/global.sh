806098f22b8a2dec0b6e4de1d08c88daa6628e75
sergrida <sergio.ridaura.garcia@gmail.com>
2022-06-26 12:10:06 +0200

    update

--- commands/git/config/global.sh
+++ commands/git/config/global.sh
@@ -5,15 +5,17 @@
 # Has 3 arguments.
 if [ $# -eq 3 ]; then
 
+  #€ Configura el usuario de Git de forma global, con los argumentos recibidos.
+  # Configure the Git user globally, with the arguments received.
+  git config --global user.name "\"${2}\""
+  git config --global user.email ${3}
+
+#€ No tiene argumentos.
+# Has no arguments.
+elif [ $# -eq 0 ]; then
+
   #€ Configura el usuario de Git de forma global, con las variables de LOOM.
   # Configure the Git user globally, with LOOM variables.
-  git config --global user.name "\"${LOOM_MYUSER}\""
-  git config --global user.email ${LOOM_EMAIL}
-
-else
-
-  #€ Configura el usuario de Git de forma global.
-  # Configure the Git user globally.
   git config --global user.name "\"${LOOM_MYUSER}\""
   git config --global user.email ${LOOM_EMAIL}
 
--- commands/git/config/local.sh
+++ commands/git/config/local.sh
@@ -5,16 +5,16 @@
 # Has 3 arguments.
 if [ $# -eq 3 ]; then
 
-  #€ Configura el usuario de Git de forma local.
-  # Configure the Git user locally.
-  git config --local user.name "\"${LOOM_MYUSER}\""
-  git config --local user.email ${LOOM_EMAIL}
+  #€ Configura el usuario de Git de forma local, con los argumentos recibidos.
+  # Configure the Git user locally, with the arguments receive
+  (cd ${PWD} && git config --local user.name "\"${2}\"")
+  (cd ${PWD} && git config --local user.email ${3})
 
 else
 
-  #€ Configura el usuario de Git de forma global.
-  # Configure the Git user globally.
-  git config --global user.name "\"${LOOM_MYUSER}\""
-  git config --global user.email ${LOOM_EMAIL}
+  #€ Configura el usuario de Git de forma local, con las variables de LOOM.
+  # Configure the Git user locally, with LOOM variables.
+  (cd ${PWD} && git config --local user.name "\"${LOOM_MYUSER}\"")
+  (cd ${PWD} && git config --local user.email ${LOOM_EMAIL})
 
 fi
