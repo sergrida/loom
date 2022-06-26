d69f05719d55201732d1abf4faab3112e7e30ec7
sergrida <sergio.ridaura.garcia@gmail.com>
2022-06-26 11:59:20 +0200

    update

--- commands/ubuntu/file/new.sh
+++ commands/ubuntu/file/new.sh
@@ -1,12 +1,3 @@
 #€ Nuevo archivo.
 # New file.
-
-#€ Tiene 2 argumentos.
-# Has 2 arguments.
-if [ $# -eq 2 ]; then
-
-  #€ Nuevo archivo.
-  # New file.
-  touch ${PWD}/${2}
-
-fi
+touch ${PWD}/${2}
--- commands/ubuntu/folder/new.sh
+++ commands/ubuntu/folder/new.sh
@@ -1,12 +1,3 @@
 #€ Nueva carpeta.
 # New folder.
-
-#€ Tiene 2 argumentos.
-# Has 2 arguments.
-if [ $# -eq 2 ]; then
-
-  #€ Nueva carpeta.
-  # New folder.
-  mkdir ${PWD}/${2}
-
-fi
+mkdir ${PWD}/${2}
