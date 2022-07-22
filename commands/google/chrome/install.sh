d0947758ed7940dd015d6f9ab1eb64686b1ac9df
sergrida <sergio.ridaura.garcia@gmail.com>
2022-07-22 08:40:27 +0200

    project

--- commands/google/chrome/install.sh
+++ commands/google/chrome/install.sh
@@ -1,5 +1,6 @@
 #@es Instala Google Chorme.
 #@en Install Google Chorme.
+sudo apt-get -y install wget
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
