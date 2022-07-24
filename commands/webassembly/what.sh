57007fb92b7e2d1fea68ddfc70a3c133ce38692f
sergrida <sergio.ridaura.garcia@gmail.com>
2022-07-24 12:20:11 +0200

    web

---
+++ commands/webassembly/what.sh
@@ -0,0 +1,4 @@
+#@es Qué es WebAssembly.
+#@en What is WebAssembly.
+echo -e "webassembly (es)     - WebAssembly, abreviado wasm, es un formato de código binario portable, para la ejecución íntegra en navegador de scripts de lado del cliente."
+echo -e "webassembly (en)     - WebAssembly, abbreviated wasm, is a portable binary code format for full browser execution of client-side scripts."
---
+++ commands/webassembly/www.sh
@@ -0,0 +1,4 @@
+#@es WebAssembly en la World Wide Web.
+#@en WebAssembly on the World Wide Web.
+echo "official website     - https://webassembly.org/"
+echo "wikipedia            - https://es.wikipedia.org/wiki/WebAssembly
