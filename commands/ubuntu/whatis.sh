3cb90cab846384364db536356c93d065cd00cbe8
sergrida <sergio.ridaura.garcia@gmail.com>
2022-07-31 20:26:57 +0200

    s

--- commands/ubuntu/whatis.sh
+++ commands/ubuntu/whatis.sh
@@ -1,44 +1,44 @@
-# Qué es el comando.
+# What is the command.
 
-# Si tiene 2 argumentos.
+# If it has 2 arguments.
 if [ $# -eq 2 ]; then
 
-  # El archivo `what` del comando existe.
+  # The command's `what` file exists.
   if [[ -f "${LOOM_COMMANDS}/${2}/what.sh" ]]; then
 
-    # Ejecuta el script `what` del comando.
+    # Execute the `what` script of the command.
     source ${LOOM_COMMANDS}/${2}/what.sh
 
   else
 
-    # Qué es la aplicación indicada.
+    # What is the indicated application.
     whatis ${2}
 
   fi
 
-# Si tiene 3 argumentos y el argumento 2 es `npm`.
+# If it has 3 arguments and argument 2 is `npm`.
 elif [ $# -eq 3 ] && [ ${2} == "npm" ]; then
 
-  # El archivo `what` del comando existe.
+  # The command's `what` file exists.
   if [[ -f "${LOOM_COMMANDS}/npm/${3}/what.sh" ]]; then
 
-    # Ejecuta el script `what` del comando.
+    # Execute the `what` script of the command.
     source ${LOOM_COMMANDS}/npm/${3}/what.sh
 
   else
 
-    # Qué es la aplicación indicada.
+    # What is the indicated application.
     echo -e "${3} (npm) \t     - $(npm view ${3} description)."
 
   fi
 
-# Si tiene 4 argumentos y el segundo es `code` y el tercero `extensions`.
+# If you have 4 arguments and the second is `code` and the third is `extensions`.
 elif [ $# -eq 4 ] && [ ${2} == "code" ] && [ ${3} == "extensions" ]; then
 
-  # El archivo `what` del comando existe.
+  # The command's `what` file exists.
   if [[ -f "${LOOM_COMMANDS}/code/extensions/${4}/what.sh" ]]; then
 
-    # Ejecuta el script `what` del comando.
+    # Execute the `what` script of the command.
     source ${LOOM_COMMANDS}/code/extensions/${4}/what.sh
 
   fi
--- commands/ubuntu/where.sh
+++ commands/ubuntu/where.sh
@@ -1,17 +1,17 @@
-# Dónde está el comando.
+# Where is the command.
 
-# Si tiene 2 argumentos.
+# If it has 2 arguments.
 if [ $# -eq 2 ]; then
 
-  # El archivo `where` del comando existe.
+  # The command `where` file exists.
   if [[ -f "${LOOM_COMMANDS}/${2}/where.sh" ]]; then
 
-    # Ejecuta el script `where` del comando.
+    # Execute the `where` script of the command.
     source ${LOOM_COMMANDS}/${2}/where.sh
 
   else
 
-    # Dónde está la aplicación indicada.
+    # Where is the indicated application.
     which ${2}
 
   fi
